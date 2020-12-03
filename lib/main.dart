c9c3c1ccfed6e4225a9171be1f2b043f05f9e334
Jux salley <juxsalley@Juxs-MacBook-Air.local>
2020-12-02 16:38:18 -0800

    q and a done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/modules/questions.dart';
 
 void main() => runApp(Quizzler());
 
@@ -19,12 +20,71 @@ class Quizzler extends StatelessWidget {
   }
 }
 
+final alert = AlertDialog(
+  title: Text('data'),
+);
+
 class QuizPage extends StatefulWidget {
   @override
   _QuizPageState createState() => _QuizPageState();
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int scoreKeeper = 0;
+  int finalScore = 0;
+
+  void reset() {
+    if (scoreKeeper == _question.length - 1) {
+      scoreKeeper = 0;
+      scoreboard.clear();
+      _showMyDialog();
+    }
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Final Score'),
+          content: Text('You Score ${finalScore}'),
+        );
+      },
+    );
+  }
+
+  List<Widget> scoreboard = [];
+
+  List<Questions> _question = [
+    Questions('Some cats are actually allergic to humans', true),
+    Questions('You can lead a cow down stairs but not up stairs.', false),
+    Questions(
+        'Approximately one quarter of human bones are in the feet.', true),
+    Questions('A slug\'s blood is green.', true),
+    Questions('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Questions('It is illegal to pee in the Ocean in Portugal.', true),
+    Questions(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Questions(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Questions(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Questions(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Questions('Google was originally called \"Backrub\".', true),
+    Questions(
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    Questions(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +97,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                _question[scoreKeeper].question,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,6 +121,17 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(() {
+                  reset();
+                  finalScore++;
+                  scoreKeeper++;
+                  scoreboard.add(Icon(
+                    Icons.done_rounded,
+                    color: Colors.blue,
+                  ));
+                  // counterCard();
+                });
+
                 //The user picked true.
               },
             ),
@@ -79,12 +150,25 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  reset();
+                  scoreKeeper++;
+                  scoreboard.add(Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  ));
+                });
+                // counterCard();
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Container(
+          height: 25.0,
+          child: Row(
+            children: scoreboard,
+          ),
+        ),
       ],
     );
   }
---
+++ lib/modules/questions.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Questions {
+  final String question;
+  final bool answer;
+
+  Questions(
+    this.question,
+    this.answer,
+  );
+}
